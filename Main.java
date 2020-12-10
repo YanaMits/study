71e45f7dcc0db05c46ca345cebd9ce82d958ebca
YanaMits <75701496+YanaMits@users.noreply.github.com>
2020-12-10 14:09:56 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,99 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello World!");
+        System.out.println("How are you?");
+
+        int x;
+        x = 12;
+        int y = 53;
+        int sum = x+y;
+        System.out.println(sum);
+
+        // String number1 = "20";
+        //int number2 = 15;
+        //System.out.println(number1 + number2);
+
+        String firstname = "Yana";
+        String lastname = "Mits";
+        String fulname = firstname + " " + lastname;
+
+        System.out.printf("%s \n", firstname);
+
+
+        System.out.println("Hello, Yana!");
+        System.out.println("Hello, Yana!");
+        System.out.println("Hello, my name is James and I am " + 40 + " years ald.");
+
+        System.out.println("This is the first line.");
+        System.out.println("This should be the second line.");
+        System.out.println("And this should be the third line.");
+
+        //This is a variable that we call "name" and its datatype is String
+       // String name = "Yana";
+//This is a variable that we call "age" and its datatype is int which means Integer
+        //int age = 32;
+       // System.out.println("Hello my name is "+name+" and I am "+ age + " years old.");
+
+        int number1 = 12345;
+        int number2 = 23456;
+        int number3 = number1 + number2;
+        System.out.println(number1 + number2);
+        System.out.println(number3);
+
+       // String name = "James";
+       // int age1 = 34;
+       // int age2 = 6;
+       // int age3 = age1 + age2;
+
+       // System.out.println(name + age3);
+
+        String name = "James ";
+        System.out.println(name + 34 + 6);
+        System.out.println(name + (34 + 6)*2);
+
+        System.out.println("Hello, World!");
+
+        String [] frase = {"Hello, World!"};
+        for (String i : frase) {
+            System.out.println(i);
+        }
+
+
+        int num1 = 7;
+        int num2 = 3;
+        int sum2 = num1 + num2;
+        System.out.println(sum2);
+        System.out.println("The result is " + sum2);
+
+        int num3 = 7;
+        int num4 = 2;
+        float result = num3 / num4;
+        System.out.println(result);
+
+        String num5 = "7";
+        String num6 = "3";
+        String sum3 = num5+num6;
+        System.out.println(sum3);
+
+
+        int num7 = 3;
+        int num8 = 2;
+        int sum4 = num7 + num8;
+        System.out.println(sum4);
+
+
+
+
+
+
+
+    }
+}
+
+
+
